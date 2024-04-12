2fdd5a602ddf379e9a0050bf504968a529b6a49b
ITJacksonClark <166760334+ITJacksonClark@users.noreply.github.com>
2024-04-12 11:29:04 -0500

    Add files via upload

---
+++ Group Project 1/Group Project 1 Examples/STR06.cpp
@@ -0,0 +1,37 @@
+// Program showing off recommendation STR06 of SEI CERT C Coding Standard
+// By Jackson Clark
+// 3/17/24
+
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+/**
+ * @brief An example program that does not assume that strtok() leaves the parse string unchanged
+ * 
+ * @return int Returns zero when the program finishes running
+ */
+
+int main(){
+    char *token;
+    char str[] = "Hello World!";
+
+    /*Temporary buffer so that we can still access the original string*/
+    char *copy = (char *)malloc(strlen(str) + 1);
+    if(copy == NULL){
+        cerr<<"The copied string is empty"<<endl;
+        exit(1);
+    }
+    strcpy(copy, str);
+    token = strtok(copy," ,");
+
+    while(token!=NULL){
+        cout<<token<<endl;
+        token = strtok(NULL," ,");
+    }
+
+    free(copy);
+    copy = NULL;
+
+    return 0;
+}
---
+++ Group Project 1/Group Project 1 Program/User.h
@@ -0,0 +1,58 @@
+// Header file for User.cpp
+// By Jackson Clark, Brian Nicollelis, Dylan Benoit, John Lagrisola, Jakob Syvertsen, & Noah Pearson 
+// 3/17/24
+
+#ifndef USER_H
+#define USER_H
+ 
+#include <string>
+#include <type_traits>
+
+namespace userNamespace { //DCL58
+    class User {
+        private:
+            std::string name;
+            int id;
+            int branch_id;
+            float balance;
+
+            long principle;
+            long rate;
+
+        public:
+            User();
+            User(std::string name, int id);
+            ~User();
+            float withdraw(float amount);
+            float deposit(float amount);
+            std::string getName();
+            int getID();
+            float getBalance();
+            int getBranchID();
+            void takeLoan(int amount, float rate);
+            long getPrinciple();
+            long getRate();
+
+            void processTransactions(int depositAmount, int withdrawalAmount); //EXP50
+            //OOP57 & EXP62
+            bool operator!=(const User &rhs) const
+            {
+                return rhs.name > name;
+            }
+    };
+}
+
+using namespace std;
+struct Printing {
+
+    void print() {
+        cout<<"This is a bank program!"<<endl;
+    }
+};
+static_assert(is_standard_layout<Printing>::value, "Printing is required to be a standard layout type");
+
+void callPrint(Printing &printing)
+{
+    printing.print();
+}
+#endif
