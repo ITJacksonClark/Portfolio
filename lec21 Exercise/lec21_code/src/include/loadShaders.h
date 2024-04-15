864aec3ee87d15da571b05411cdd0249cf69fc1d
ITJacksonClark <166760334+ITJacksonClark@users.noreply.github.com>
2024-04-15 01:03:00 -0500

    Add files via upload

---
+++ lec21 Exercise/lec21_code/src/include/loadShaders.h
@@ -0,0 +1,15 @@
+// Header file for loadShaders.cpp
+// By Dr. Qi Zhang
+// 3/25/24
+
+#ifndef __LOADSHADER_MAT_H__
+#define __LOADSHADER_MAT_H__
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+static char* readShaderSource(const char* shaderFile);
+int loadShader(const char* vShaderFile, const char* fShaderFile);
+
+#endif
---
+++ lec21 Exercise/lec21_code/src/shader/2.2.3n.lamp.frag
@@ -0,0 +1,11 @@
+// Fragment shader for example2.cpp
+// By Dr. Qi Zhang
+// 3/25/24
+
+#version 460 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0); // set alle 4 vector values to 1.0
+}
